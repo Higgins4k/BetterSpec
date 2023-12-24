a80cd1557deba675d4cbb216f89f39a1a052816a
Higgins4k <56009828+Higgins4k@users.noreply.github.com>
2023-12-24 02:21:37 -0500

    Add project files.

---
+++ BetterSpec/Patches/PlayerControllerBPatches.cs
@@ -0,0 +1,36 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BetterSpec.Patches
+{
+    
+    internal class HUDManagerPatches
+    {
+        [HarmonyPatch(typeof(HUDManager), "Update")]
+        [HarmonyPrefix]
+        static void specClockPatch(HUDManager __instance)
+        {
+            if (__instance == null)
+            {
+                return;
+            }
+
+            if (GameNetworkManager.Instance.localPlayerController.isPlayerDead)
+            {
+                if (__instance.Clock != null)
+                {
+                    HUDManager.Instance.HideHUD(false);
+                    HUDManager.Instance.SetClockVisible(true);
+                    HUDManager.Instance.Clock.targetAlpha = 1f;
+                   
+
+                }
+                else
+                {
+                    Debug.LogError("Clock in HUDManager is null!");
+                }
+            }
+        }
+    }
+}
---
+++ BetterSpec/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BetterSpec.Patches;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterSpec
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    public class MainBetterSpec : BaseUnityPlugin
+    {
+
+
+        private const string modGUID = "zg.BetterSpec";
+        private const string modName = "BetterSpec";
+        private const string modVersion = "1.0.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static MainBetterSpec instance;
+
+        internal ManualLogSource pnt;
+
+
+
+        void Awake()
+        {
+
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            pnt = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            pnt.LogInfo("BetterSpec Enabled");
+            pnt.LogInfo("Make sure to review BetterSpec");
+
+            harmony.PatchAll(typeof(MainBetterSpec));
+            harmony.PatchAll(typeof(HUDManagerPatches));
+        }
+
+    }
+}
